e8a0bf3b4fe8993d6a1a5002cdfddf9a887c11ec
JohnnyDevNull <philipp.john@live.de>
2024-05-07 15:13:24 +0200

    Release 0.1

--- src/app/utils/has-change.ts
+++
@@ -1,5 +0,0 @@
-import { SimpleChange } from '@angular/core';
-
-export function hasChange(change: SimpleChange) {
-  return change?.previousValue != null && change.previousValue !== change.currentValue;
-}
