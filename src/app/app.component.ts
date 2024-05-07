53b1a3e23cdcbb4b1d6adeb6b725be8d30ba855c
JohnnyDevNull <philipp.john@live.de>
2024-05-07 14:19:03 +0200

    feat: add timer update interval

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, inject, OnInit } from '@angular/core'
+import { Component, inject, OnDestroy, OnInit } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { formatDuration } from 'date-fns/formatDuration';
 import { intervalToDuration } from 'date-fns/intervalToDuration';
 import { isBefore } from 'date-fns/isBefore';
+import { interval, Subject, takeUntil } from 'rxjs';
 import { CountdownDataComponent } from './components/countdown-data/countdown-data.component';
 import { UserInputComponent } from './components/user-input/user-input.component';
 import { LocalStorageService } from './services/local-storage.service';
@@ -15,20 +16,35 @@ import { shortEnLocale } from './utils/short-en-locale';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   titleValue = '';
   durationValue = '';
 
   private readonly storageService = inject(LocalStorageService);
+
+  private dateValue: Date | null = null;
+  private destroy$ = new Subject<void>();
 
   ngOnInit() {
     const data = this.storageService.get();
     if (data) {
       this.titleValue = data.title as string;
       if (data.date) {
-        this.durationValue = this.toDuration(new Date(data.date));
+        this.dateValue = new Date(data.date);
+        this.durationValue = this.toDuration(new Date(this.dateValue));
       }
     }
+
+    interval(1000).pipe(takeUntil(this.destroy$)).subscribe(() => {
+      if (this.dateValue) {
+        this.durationValue = this.toDuration(new Date(this.dateValue));
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   onTitleChange(value: string) {
@@ -37,7 +53,8 @@ export class AppComponent implements OnInit {
   }
 
   onDateChange(value: Date | null) {
-    this.durationValue = this.toDuration(value)
+    this.dateValue = value;
+    this.durationValue = this.toDuration(value);
     this.storageService.save({ date: value?.toISOString() });
   }
 
